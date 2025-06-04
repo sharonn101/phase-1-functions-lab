77e3825e988033bd5edfaaddde57843b2d90ca24
Sharon <sharonirungu2006@gmail.com>
2025-06-05 01:02:44 +0300

    adds functions

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const headquartersLocation = 42;
+function distanceFromHqInBlocks(pickUpLocation) {
+  if (pickUpLocation >= headquartersLocation) {
+    return pickUpLocation - headquartersLocation;
+  } else {
+    return headquartersLocation - pickUpLocation;
+  }
+}
+function distanceFromHqInFeet(pickUpLocation) {
+  const distanceInBlocks = distanceFromHqInBlocks(pickUpLocation);
+  return distanceInBlocks * 264;
+}
+function distanceTravelledInFeet(headquartersLocation, pickUpLocation) {
+    distanceTravelledInFeet = Math.abs(pickUpLocation - headquartersLocation)  * 264;
+    return distanceTravelledInFeet;
+}
+function calculatesFarePrice(start, destination) {
+  const feetPerBlock = 264;
+  const distanceInFeet = Math.abs(destination - start) * feetPerBlock;
+
+  if (distanceInFeet <= 400) {
+    return 0;
+  } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+    return (distanceInFeet - 400) * 0.02;
+  } else if (distanceInFeet > 2000 && distanceInFeet <= 2500) {
+    return 25;
+  } else {
+    return 'cannot travel that far';
+  }
+}
